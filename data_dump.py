afb9e81aff2d1ccc986738a80c6da8b06afc9bb0
ct-kt <ct4g00gle@gmail.com>
2022-12-14 14:42:55 +0530

    another update

--- data_dump.py
+++ data_dump.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
 
     df.reset_index(drop=True, inplace=True)
     json_record = list(json.loads(df.T.to_json()).values())
-    print(json_record[0])
-
+    
     #insert converted json record to monga DB
     client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
